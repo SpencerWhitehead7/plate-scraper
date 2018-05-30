62be976f3afd8837f42fbbccff467c4278d6da60
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-05-30 16:09:32 -0400

    refactors

--- client/index.js
+++ client/index.js
@@ -5,9 +5,11 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
+import Main from './components'
+
 ReactDOM.render(
 	<Provider store={store}>
-		<div>boilerplate</div>
+		<Main/>
 	</Provider>,
 	document.getElementById(`root`)
 )
