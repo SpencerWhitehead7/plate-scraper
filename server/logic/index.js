0b6a782811e353b944df09339dc15bf102aa6acc
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-05-31 16:34:30 -0400

    refactors

--- server/logic/index.js
+++ server/logic/index.js
@@ -2,22 +2,22 @@ const requestPromiseNative = require(`request-promise-native`)
 const cheerio = require(`cheerio`)
 
 // Parsers
-const seriousEats = require(`./seriousEats`)
 const allrecipes = require(`./allrecipes`)
-const epicurious = require(`./epicurious`)
-const thekitchn = require(`./thekitchn`)
+const bettycrocker = require(`./bettycrocker`)
 const bonappetit = require(`./bonappetit`)
+const chowhound = require(`./chowhound`)
+const cookinglight = require(`./cookinglight`)
+const eatingwell = require(`./eatingwell`)
+const epicurious = require(`./epicurious`)
+const food52 = require(`./food52`)
+const foodandwine = require(`./foodandwine`)
 const foodnetwork = require(`./foodnetwork`)
 const geniuskitchenOrfood = require(`./geniuskitchenOrfood`)
-const chowhound = require(`./chowhound`)
-const simplyrecipes = require(`./simplyrecipes`)
-const cookinglight = require(`./cookinglight`)
-const bettycrocker = require(`./bettycrocker`)
-const food52 = require(`./food52`)
 const jamieoliver = require(`./jamieoliver`)
-const eatingwell = require(`./eatingwell`)
-const foodandwine = require(`./foodandwine`)
 const myrecipes = require(`./myrecipes`)
+const seriousEats = require(`./seriousEats`)
+const simplyrecipes = require(`./simplyrecipes`)
+const thekitchn = require(`./thekitchn`)
 
 class Page{
 	constructor(url){
@@ -48,46 +48,44 @@ const scrape = url => {
 			const parserLoader = parser => parser(recipe, html)
 			// Deals with the edge case seriousEats pages
 			if(page.uri.includes(`seriouseats.com`) && !page.uri.includes(`seriouseats.com/recipes`)){
-				console.log(`Make sure your URL is at seriouseats.com/recipes, not just seriouseats.com`)
-
+				return `Make sure your URL is at seriouseats.com/recipes, not just seriouseats.com`
 			// Clauses to let you use different parsers for different websites
-			}else if(page.uri.includes(`seriouseats.com/recipes`)){ // SeriousEats
-				parserLoader(seriousEats)
-			}else if(page.uri.includes(`allrecipes.com`)){ // Allrecipes
+			}else if(page.uri.includes(`allrecipes.com`)){ // allrecipes
 				parserLoader(allrecipes)
-			}else if(page.uri.includes(`epicurious.com`)){ // Epicurious
-				parserLoader(epicurious)
-			}else if(page.uri.includes(`thekitchn.com`)){ // theKitchn
-				parserLoader(thekitchn)
+			}else if(page.uri.includes(`bettycrocker.com`)){ // bettycrocker
+				parserLoader(bettycrocker)
 			}else if(page.uri.includes(`bonappetit.com`)){ // bonappetit
 				parserLoader(bonappetit)
+			}else if(page.uri.includes(`chowhound.com`)){ // chowhound
+				parserLoader(chowhound)
+			}else if(page.uri.includes(`cookinglight.com`)){ // cookinglight
+				parserLoader(cookinglight)
+			}else if(page.uri.includes(`eatingwell.com`)){ // eatingwell
+				parserLoader(eatingwell)
+			}else if(page.uri.includes(`epicurious.com`)){ // epicurious
+				parserLoader(epicurious)
+			}else if(page.uri.includes(`food52.com`)){ // food52
+				parserLoader(food52)
+			}else if(page.uri.includes(`foodandwine.com`)){ // foodandwine
+				parserLoader(foodandwine)
 			}else if(page.uri.includes(`foodnetwork.com`)){ // foodnetwork
 				parserLoader(foodnetwork)
 			}else if(page.uri.includes(`geniuskitchen.com`)){ // geniuskitchen/food
 				parserLoader(geniuskitchenOrfood)
-			}else if(page.uri.includes(`chowhound.com`)){ // chowhound
-				parserLoader(chowhound)
-			}else if(page.uri.includes(`simplyrecipes.com`)){ // simplyrecipes
-				parserLoader(simplyrecipes)
-			}else if(page.uri.includes(`cookinglight.com`)){ // cookinglight
-				parserLoader(cookinglight)
-			}else if(page.uri.includes(`bettycrocker.com`)){ // bettycrocker
-				parserLoader(bettycrocker)
-			}else if(page.uri.includes(`food52.com`)){ // food52
-				parserLoader(food52)
 			}else if(page.uri.includes(`jamieoliver.com`)){ // jamieoliver
 				parserLoader(jamieoliver)
-			}else if(page.uri.includes(`eatingwell.com`)){ // eatingwell
-				parserLoader(eatingwell)
-			}else if(page.uri.includes(`foodandwine.com`)){ // foodandwine
-				parserLoader(foodandwine)
 			}else if(page.uri.includes(`myrecipes.com`)){ // myrecipes
 				parserLoader(myrecipes)
+			}else if(page.uri.includes(`seriouseats.com/recipes`)){ // seriouseats
+				parserLoader(seriousEats)
+			}else if(page.uri.includes(`simplyrecipes.com`)){ // simplyrecipes
+				parserLoader(simplyrecipes)
+			}else if(page.uri.includes(`thekitchn.com`)){ // thekitchn
+				parserLoader(thekitchn)
 			}else{
-				console.log(`Sorry, we don't support that website`)
+				return `Sorry, we don't support that website`
 			}
 			const recipeStr = `Source: ${page.uri}\n\n${recipeToStr(recipe)}`
-			console.log(recipeStr)
 			return recipeStr
 		})
 	/* eslint-enable complexity */
@@ -95,5 +93,3 @@ const scrape = url => {
 }
 
 module.exports = scrape
-
-scrape(`http://www.myrecipes.com/recipe/bananas-foster-bread`)
