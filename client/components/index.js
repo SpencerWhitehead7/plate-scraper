46e922d83e617a4d1dd40b8e705dd56a552bf150
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-05-30 22:48:54 -0400

    adds basic form  and textarea for editing

--- client/components/index.js
+++ client/components/index.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Main = () => {
-	return (
-		<div>
-        Boilerplate
-		</div>
-	)
+class Main extends Component{
+	constructor(){
+		super()
+		this.state = {
+			url : ``,
+			recipe : `AAA`,
+		}
+	}
+
+  handleChange = event => {
+  	this.setState({
+  		[event.target.name] : event.target.value,
+  	})
+  }
+
+  handleSubmit = event => {
+  	event.preventDefault()
+  	console.log(event.target)
+  	// axios??? some command to backend to make it run the script
+  }
+
+  render(){
+  	return (
+  		<div>
+  			<form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+  				<label htmlFor="url">url to scrape</label>
+  				<input type="text" name="url" value={this.state.url}/>
+  				<button
+  					type="submit"
+  					disabled={false} // add conditionals for if scraping the site would fail (ie, not supported)
+  				>
+          Scrape!
+  				</button>
+  			</form>
+  			{
+  				this.state.recipe === ``
+  					? <div/>
+  					: <textarea>
+  						  {this.state.recipe}
+  					  </textarea>
+  			}
+  		</div>
+  	)
+  }
 }
 
 export default Main
