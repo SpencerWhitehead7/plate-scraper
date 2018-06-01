61fbd12065322f09d72d6c2d900e573deff2fbaf
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-01 17:17:08 -0400

    refactors jsx/css

--- client/components/index.js
+++ client/components/index.js
@@ -46,46 +46,41 @@ handleChange = event => {
   		<div id="whole-page">
   			<h1 id="title">Plate Scraper!</h1>
   			<SupportedSites/>
-  					<form onChange={this.handleChange} onSubmit={this.handleSubmit} id="input-form">
-  					<label htmlFor="url">
-            Paste in a recipe url from a supported site and Scrape!<br/>
-  						{
-  						  (this.state.url.includes(`seriouseats.com`) && !this.state.url.includes(`seriouseats.com/recipes`))
-  								? <span className="warning">{`Make sure your url is from seriouseats.com/recipes, not just seriouseats.com`}</span>
-  								: <span/>
-  						}
-  					</label>
-  				<div id="input-button-container">
-  					<input
-  							type="text"
-  							name="url"
-  							placeholder="url to scrape: for example, https://www.allrecipes.com/recipe/22918/pop-cake/"
-  							value={this.state.url}
-  							id="input"
+  			<form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+  				<label htmlFor="url">
+        Paste in a recipe url from a supported site and Scrape!<br/>
+  					{
+  						(this.state.url.includes(`seriouseats.com`) && !this.state.url.includes(`seriouseats.com/recipes`))
+  							? <span className="warning">{`Make sure your url is from seriouseats.com/recipes, not just seriouseats.com`}</span>
+  							: null
+  					}
+  				</label>
+  				<input
+  					type="text"
+  					name="url"
+  					placeholder="url to scrape: for example, https://www.allrecipes.com/recipe/22918/pop-cake/"
+  					value={this.state.url}
+  					id="input"
+  				/>
+  				<SubmitButton url={this.state.url}/>
+  			</form>
+  			{
+  				this.state.recipe === ``
+  					? null
+  					:	<Textarea
+  						name="recipe"
+  						onChange={this.handleChange}
+  						value={this.state.recipe}
+  						id="recipe-textarea"
   					/>
-  						<SubmitButton url={this.state.url}/>
-  				</div>
-  					</form>
-  			<div id="textarea-error-container">
-  				{
-  					this.state.recipe === ``
-  						? <div/>
-  						:	<Textarea
-  							name="recipe"
-  							onChange={this.handleChange}
-  							value={this.state.recipe}
-  							id="recipe-textarea"
-  						/>
-  				}
-  				{
-  					this.state.error
-  						? <div className="warning">
-                  Error: failed to scrape: invalid url<br/>
-                  Make sure you're using the url of a specific recipe
-  						  </div>
-  						: <div/>
-  				}
-  			</div>
+  			}{
+  				this.state.error
+  					? <div className="warning">
+                Error: failed to scrape: invalid url<br/>
+                Make sure you're using the url of a specific recipe
+  					  </div>
+  					: null
+  			}
   		</div>
   	)
   }
