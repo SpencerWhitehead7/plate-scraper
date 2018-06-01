91f147cf7083151e41f2b6f6e561e2b164280a05
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-01 12:47:53 -0400

    edits to remove trailing PC lines

--- server/logic/foodnetwork.js
+++ server/logic/foodnetwork.js
@@ -7,6 +7,9 @@ const foodnetwork = (recipe, html) => {
 	html(`.o-Method__m-Body p`).each(function(){
 		recipe.instructions.push(`${html(this).text().trim()}`)
 	})
+	if(recipe.instructions[recipe.instructions.length - 1].includes(`Photographs by`)){ // to deal with the many FN.com recipes where the final "instruction" is a PC
+		recipe.instructions = recipe.instructions.slice(0, -1) // this is an ugly and graceless fix, but they have ugly and graceless html and I'm doing the best I can
+	}
 }
 
 module.exports = foodnetwork
