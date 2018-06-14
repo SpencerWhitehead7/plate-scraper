0984e4d940918c236919df79ad26b66f3bd7c6a4
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-14 11:46:12 -0400

    linterfix

--- client/components/index.js
+++ client/components/index.js
@@ -6,83 +6,83 @@ import SupportedSites from './SupportedSites'
 import SubmitButton from './SubmitButton'
 
 class Main extends React.Component{
-	constructor(){
-		super()
-		this.state = {
-			url : ``,
-			recipe : ``,
-			error : false,
-		}
-	}
+  constructor(){
+    super()
+    this.state = {
+      url : ``,
+      recipe : ``,
+      error : false,
+    }
+  }
 
 handleChange = event => {
-	this.setState({
-		[event.target.name] : event.target.value,
-	})
+  this.setState({
+    [event.target.name] : event.target.value,
+  })
 }
 
   handleSubmit = event => {
-  	event.preventDefault()
-  	axios.post(`/api`, {url : this.state.url})
-  	.then(recipe => {
-  		if(!recipe.data.includes(`•`) || !recipe.data.includes(`1)`)){
-  			this.setState({
-  				error : true,
-  				recipe : ``,
-  			})
-  		}else{
-  		  this.setState({
-  			  url : ``,
-  				recipe : recipe.data,
-  				error : false,
-  			})
-  		}
-  	})
-  		.catch(error => console.log(error))
+    event.preventDefault()
+    axios.post(`/api`, {url : this.state.url})
+      .then(recipe => {
+        if(!recipe.data.includes(`•`) || !recipe.data.includes(`1)`)){
+          this.setState({
+            error : true,
+            recipe : ``,
+          })
+        }else{
+          this.setState({
+            url : ``,
+            recipe : recipe.data,
+            error : false,
+          })
+        }
+      })
+      .catch(error => console.log(error))
   }
 
   render(){
-  	return (
-  		<div id="whole-page">
-  			<h1 id="title">Plate Scraper!</h1>
-  			<SupportedSites/>
-  			<form onChange={this.handleChange} onSubmit={this.handleSubmit}>
-  				<label htmlFor="url">
+    return (
+      <div id="whole-page">
+        <h1 id="title">Plate Scraper!</h1>
+        <SupportedSites/>
+        <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+          <label htmlFor="url">
         Paste in a recipe url from a supported site and Scrape!<br/>
-  					{
-  						(this.state.url.includes(`seriouseats.com`) && !this.state.url.includes(`seriouseats.com/recipes`))
-  							? <span className="warning">{`Make sure your url is from seriouseats.com/recipes, not just seriouseats.com`}</span>
-  							: null
-  					}
-  				</label>
-  				<input
-  					type="text"
-  					name="url"
-  					placeholder="url to scrape: for example, https://www.allrecipes.com/recipe/22918/pop-cake/"
-  					value={this.state.url}
-  					id="input"
-  				/>
-  				<SubmitButton url={this.state.url}/>
-  			</form>
-  			{
-  				this.state.recipe === ``
-  					? null
-  					:	<Textarea
-  						name="recipe"
-  						onChange={this.handleChange}
-  						value={this.state.recipe}
-  						id="recipe-textarea"
-  					/>
-  			}{
-  				this.state.error
-  					? <div className="warning">
+            {
+              (this.state.url.includes(`seriouseats.com`) && !this.state.url.includes(`seriouseats.com/recipes`))
+                ? <span className="warning">{`Make sure your url is from seriouseats.com/recipes, not just seriouseats.com`}</span>
+                : null
+            }
+          </label>
+          <input
+            type="text"
+            name="url"
+            placeholder="url to scrape: for example, https://www.allrecipes.com/recipe/22918/pop-cake/"
+            value={this.state.url}
+            id="input"
+          />
+          <SubmitButton url={this.state.url}/>
+        </form>
+        {
+          this.state.recipe === ``
+            ? null
+            :	<Textarea
+              name="recipe"
+              onChange={this.handleChange}
+              value={this.state.recipe}
+              id="recipe-textarea"
+            />
+        }{
+          this.state.error
+            ? <div className="warning">
                 Error: failed to scrape: invalid url<br/>
                 Make sure you're using the url of a specific recipe
-  					  </div>
-  					: null
-  			}
-  		</div>
-  	)
+            </div>
+            : null
+        }
+      </div>
+    )
   }
 }
 
