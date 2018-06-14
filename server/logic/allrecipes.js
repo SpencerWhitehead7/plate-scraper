4b9ccc8f08d4a7450e759a9466bf270afe466f96
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-14 11:42:39 -0400

    fixes accidentally pulling in-line ads

--- server/logic/allrecipes.js
+++ server/logic/allrecipes.js
@@ -1,13 +1,13 @@
 const allrecipes = (recipe, html) => {
-	recipe.title = (html(`h1`).text().trim())
-	html(`.checkList__line`).each(function(){
-		recipe.ingredients.push(`• ${html(this).text().trim()}`)
-	})
-	recipe.ingredients = recipe.ingredients.slice(0, -3) // to deal with some html BS
-	html(`.recipe-directions__list--item`).each(function(){
-		recipe.instructions.push(`${html(this).text().trim()}`)
-	})
-	recipe.instructions = recipe.instructions.slice(0, -1) // to deal with some html BS
+  recipe.title = (html(`h1`).text().trim())
+  html(`.checkList__line label`).each(function(){
+    recipe.ingredients.push(`• ${html(this).text().trim()}`)
+  })
+  recipe.ingredients = recipe.ingredients.slice(0, -3) // to deal with some html BS
+  html(`.recipe-directions__list--item`).each(function(){
+    recipe.instructions.push(`${html(this).text().trim()}`)
+  })
+  recipe.instructions = recipe.instructions.slice(0, -1) // to deal with some html BS
 }
 
 module.exports = allrecipes
