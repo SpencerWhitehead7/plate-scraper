587efcedf021080517c331b11f5d6ed77071ec8f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-06-17 15:52:57 -0400

    remove: get all users route

--- server/api/user.js
+++ server/api/user.js
@@ -1,16 +1,6 @@
 const router = require(`express`).Router()
 const { User } = require(`../db`)
 
-// GET /api/user
-router.get(`/`, async (req, res, next) => {
-  try{
-    const users = await User.findAll()
-    res.json(users)
-  }catch(error){
-    next(error)
-  }
-})
-
 // GET /api/user/:wildcard
 router.get(`/:id`, async (req, res, next) => {
   try{
