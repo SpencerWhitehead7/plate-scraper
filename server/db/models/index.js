d2fb64c7213805b92f9f0fc3af9b8690ee1c6b7d
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-05 01:08:58 -0500

    feat: relations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,8 +3,13 @@ const Recipe = require(`./recipe`)
 const Tag = require(`./tag`)
 
 // Associations
+// One-Many relation between Users and Recipes
 Recipe.belongsTo(User)
 User.hasMany(Recipe)
+
+// Many-Many relation between Recipes and Tags
+Tag.belongsToMany(Recipe, {through : `recipeTraits`})
+Recipe.belongsToMany(Tag, {through : `recipeTraits`})
 
 module.exports = {
   User,
