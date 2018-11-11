50ebe615254bf36c97bb1720144fca696667eeb9
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-11 01:58:46 -0500

    refactor: tests use chai-as-promised instead of jank promise wrapper

--- test/database/tag.spec.js
+++ test/database/tag.spec.js
@@ -1,12 +1,12 @@
-/* eslint-disable init-declarations */
-
-const {expect} = require(`chai`)
+const chai = require(`chai`)
+const chaiAsPromised = require(`chai-as-promised`)
 const db = require(`../../server/db`)
 const Tag = db.model(`tag`)
 
-const {SUCCESS, ERROR, createTestInstance} = require(`./logic`)
+chai.use(chaiAsPromised)
+const expect = chai.expect
 
-describe(`Tag model`, () => {
+describe(`The Tag model`, () => {
   beforeEach(async () => {
     try{
       await Tag.sync({force : true})
@@ -14,45 +14,27 @@ describe(`Tag model`, () => {
       console.log(err)
     }
   })
-
-  describe(`The Tag model`, () => {
-    it(`the model exists`, () => expect(Tag).not.to.be.an(`undefined`))
-  })
+  it(`exists`, () => expect(Tag).not.to.be.an(`undefined`))
 
   describe(`Each desired field exists`, () => {
-    let test
+    let test = null
     before(async () => {
-      test = await createTestInstance(Tag, SUCCESS, [`name`, `tagname`])
+      try{
+        test = await Tag.create({name : `tagname`})
+      }catch(err){
+        console.log(err)
+      }
     })
-    it(`has a name field`, () => expect(test.name).not.to.be.an(`undefined`))
+    it(`name`, () => expect(test.name).not.to.be.an(`undefined`))
   })
 
   describe(`Each field validates and accepts only the correct data types`, () => {
-    describe(`Name Field`, () => {
-      it(`name field is required`, async () => {
-        const test = await createTestInstance(Tag, ERROR)
-        expect(test.errors[0].message).to.equal(`tag.name cannot be null`)
-      })
-      it(`name field accepts only strings`, async () => {
-        const test = await createTestInstance(Tag, ERROR, [`name`, []])
-        expect(test.errors[0].message).to.equal(`name cannot be an array or an object`)
-      })
-      it(`name field does not accept empty strings`, async () => {
-        const test = await createTestInstance(Tag, ERROR, [`name`, ``])
-        expect(test.errors[0].message).to.equal(`Validation notEmpty on name failed`)
-      })
-      it(`name field does not accept strings with numeric (non-alphabetical) characters`, async () => {
-        const test = await createTestInstance(Tag, ERROR, [`name`, `a7a`])
-        expect(test.errors[0].message).to.equal(`Validation isAlpha on name failed`)
-      })
-      it(`name field does not accept strings with symbol (non-alphabetical) characters`, async () => {
-        const test = await createTestInstance(Tag, ERROR, [`name`, `a&a`])
-        expect(test.errors[0].message).to.equal(`Validation isAlpha on name failed`)
-      })
-      it(`name field does not accept strings with uppercase characters`, async () => {
-        const test = await createTestInstance(Tag, ERROR, [`name`, `aAa`])
-        expect(test.errors[0].message).to.equal(`Validation isLowercase on name failed`)
-      })
+    describe(`Name`, () => {
+      it(`is required`, () => expect(Tag.create({})).to.be.rejectedWith(`notNull Violation: tag.name cannot be null`))
+      it(`accepts only strings`, () => expect(Tag.create({name : []})).to.be.rejectedWith(`string violation: name cannot be an array or an object`))
+      it(`does not accept empty strings`, () => expect(Tag.create({name : ``})).to.be.rejectedWith(`Validation error: Validation notEmpty on name failed`))
+      it(`does not accept strings with non-alpha characters`, () => expect(Tag.create({name : `a7a`})).to.be.rejectedWith(`Validation error: Validation isAlpha on name failed`))
+      it(`does not accept strings with uppercase characters`, () => expect(Tag.create({name : `aAa`})).to.be.rejectedWith(`Validation error: Validation isLowercase on name failed`))
     })
   })
 })
