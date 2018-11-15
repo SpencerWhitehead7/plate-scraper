9822f96054b3b920da80e453a9dda94d4c789fea
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-15 03:58:16 -0500

    refactor: simplifies route auth, updates delete method for cascading

--- server/api/user.js
+++ server/api/user.js
@@ -3,15 +3,37 @@ const {User, Recipe} = require(`../db/models`)
 
 const {isAuthenticated} = require(`../authenticationLogic`)
 
-const isOwner = (req, res, next) => {
-  if(Number(req.params.id) === req.user.id){
-    next()
-  }else{
-    const error = new Error(`Permission denied`)
-    error.status = 401
-    next(error)
-  }
-}
+// PUT /api/user
+router.put(`/`,
+  isAuthenticated,
+  async (req, res, next) => {
+    try{
+      const userInfo = JSON.parse(JSON.stringify(req.body))
+      delete userInfo.salt
+      const [, user] = await User.update(userInfo, {
+        where : {id : req.user.id},
+        returning : true,
+        plain : true,
+      })
+      res.json(user)
+    }catch(error){
+      next(error)
+    }
+  })
+
+// DELETE /api/user
+router.delete(`/`,
+  isAuthenticated,
+  async (req, res, next) => {
+    try{
+      const user = await User.findByPk(req.user.id)
+      await user.destroy()
+      req.logout()
+      req.session.destroy(err => err ? next(err) : res.redirect(`/`))
+    }catch(error){
+      next(error)
+    }
+  })
 
 // GET /api/user/:id
 router.get(`/:id`, async (req, res, next) => {
@@ -22,38 +44,5 @@ router.get(`/:id`, async (req, res, next) => {
     next(error)
   }
 })
-
-// PUT /api/user/:id
-router.put(`/:id`,
-  isAuthenticated,
-  isOwner,
-  async (req, res, next) => {
-    try{
-      const userInfo = JSON.parse(JSON.stringify(req.body))
-      delete userInfo.salt
-      const [, user] = await User.update(userInfo, {
-        where : {id : req.params.id},
-        returning : true,
-        plain : true,
-      })
-      res.json(user)
-    }catch(error){
-      next(error)
-    }
-  })
-
-// DELETE /api/user/:id
-router.delete(`/:id`,
-  isAuthenticated,
-  isOwner,
-  async (req, res, next) => {
-    try{
-      await User.destroy({where : {id : req.params.id}})
-      req.logout()
-      req.session.destroy(err => err ? next(err) : res.redirect(`/`))
-    }catch(error){
-      next(error)
-    }
-  })
 
 module.exports = router
