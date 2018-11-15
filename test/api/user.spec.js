2732f54bf110a819b599ddae5ffb8de8fd49d6ca
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-15 04:01:21 -0500

    refactor: tests for new api routing logic

--- test/api/user.spec.js
+++ test/api/user.spec.js
@@ -7,37 +7,43 @@ const User = require(`../../server/db`).model(`user`)
 
 const agent1 = request.agent(app)
 const agent2 = request.agent(app)
-const agent3 = request.agent(app)
 
 describe(`API Route User: /api/user`, () => {
   const userCred = {email : `testUser@example.com`, password : `pw`}
   const user2Cred = {email : `testUser2@example.com`, password : `pw`}
-  const user3Cred = {email : `testUser3@example.com`, password : `pw`}
 
   before(async () => {
     try{
       await Recipe.sync({force : true})
       await User.sync({force : true})
-      await agent1.post(`/auth/signup`).send(userCred)
-      await agent2.post(`/auth/signup`).send(user2Cred)
-      await agent3.post(`/auth/signup`).send(user3Cred)
-      await agent1.post(`/api/recipe`).send({
-        text : `testText1`,
-        title : `title1`,
-      })
-      await agent1.post(`/api/recipe`).send({
-        text : `testText2`,
-        title : `title2`,
-      })
+      await Promise.all([
+        agent1.post(`/auth/signup`).send(userCred),
+        agent2.post(`/auth/signup`).send(user2Cred),
+      ])
+      await Promise.all([
+        agent1.post(`/api/recipe`).send({
+          text : `text1`,
+          title : `title1`,
+        }),
+        agent1.post(`/api/recipe`).send({
+          text : `text2`,
+          title : `title2`,
+        }),
+        agent2.post(`/api/recipe`).send({
+          text : `text3`,
+          title : `title3`,
+        }),
+      ])
     }catch(err){
       console.log(err)
     }
   })
   after(async () => {
     try{
-      await agent1.post(`/auth/logout`)
-      await agent2.post(`/auth/logout`)
-      await agent3.post(`/auth/logout`)
+      await Promise.all([
+        agent1.post(`/auth/logout`),
+        agent2.post(`/auth/logout`),
+      ])
       await Recipe.sync({force : true})
       await User.sync({force : true})
     }catch(err){
@@ -45,26 +51,7 @@ describe(`API Route User: /api/user`, () => {
     }
   })
 
-  describe(`/:id`, () => {
-    describe(`GET`, () => {
-      let res = null
-      before(async () => {
-        try{
-          res = await request(app).get(`/api/user/1`)
-        }catch(error){
-          console.log(error)
-        }
-      })
-
-      it(`returns the user`, () => {
-        expect(res.status).to.equal(200)
-        expect(res.body.email).to.equal(`testUser@example.com`)
-      })
-      it(`and all their recipes`, () => {
-        expect(res.body.recipes.length).to.equal(2)
-      })
-    })
-
+  describe(`/`, () => {
     describe(`PUT`, () => {
       let res = null
       let oldSalt = null
@@ -73,7 +60,7 @@ describe(`API Route User: /api/user`, () => {
         try{
           let user = await User.findOne({where : {email : `testUser@example.com`}})
           oldSalt = user.dataValues.salt
-          res = await agent1.put(`/api/user/1`).send({
+          res = await agent1.put(`/api/user`).send({
             email : `new@example.com`,
             salt : `new salt`,
           })
@@ -84,25 +71,18 @@ describe(`API Route User: /api/user`, () => {
         }
       })
 
-      it(`edits a user`, async () => {
+      it(`edits the logged in user`, async () => {
         const user = await User.findOne({where : {email : `new@example.com`}})
         expect(user).not.to.be.a(`null`)
       })
-      it(`rejects unauthenticated users' attempts`, async () => {
-        const failedRes = await request(app).put(`/api/user/1`).send({email : `failed@example.com`})
+      it(`rejects unauthenticated users`, async () => {
+        const failedRes = await request(app).put(`/api/user`).send({email : `failed@example.com`})
         const user = await User.findOne({where : {email : `failed@example.com`}})
         expect(failedRes.status).to.equal(401)
         expect(failedRes.text).to.equal(`Not logged in`)
         expect(user).to.be.a(`null`)
       })
-      it(`rejects attempts to edit users other than the logged in user`, async () => {
-        const failedRes = await agent2.put(`/api/user/1`).send({email : `failed@example.com`})
-        const user = await User.findOne({where : {email : `failed@example.com`}})
-        expect(failedRes.status).to.equal(401)
-        expect(failedRes.text).to.equal(`Permission denied`)
-        expect(user).to.be.a(`null`)
-      })
-      it(`does not allow you to edit the user's salt`, () => {
+      it(`does not allow the user to edit salt`, () => {
         expect(oldSalt).to.equal(newSalt)
       })
       it(`returns the user`, () => {
@@ -116,37 +96,41 @@ describe(`API Route User: /api/user`, () => {
       let res = null
       before(async () => {
         try{
-          res = await agent1.delete(`/api/user/1`)
+          res = await agent2.delete(`/api/user`)
         }catch(error){
           console.log(error)
         }
       })
-      it(`deletes a user`, async () => {
-        const user = await User.findByPk(1)
+      it(`deletes the logged in user and their recipes`, async () => {
+        const user = await User.findByPk(2)
+        const remaining = await Recipe.findAll()
         expect(user).to.be.a(`null`)
+        expect(remaining.length).to.equal(2)
       })
       it(`logs the user out`, async () => {
-        const failedRes = await agent1.get(`/auth/me`)
+        const failedRes = await agent2.get(`/auth/me`)
         expect(failedRes.status).to.equal(401)
         expect(failedRes.text).to.equal(`Not logged in`)
       })
-      it(`rejects unauthenticated users' attempts`, async () => {
-        const failedRes = await request(app).delete(`/api/user/2`)
-        const user = await User.findByPk(2)
+      it(`rejects unauthenticated users`, async () => {
+        const failedRes = await request(app).delete(`/api/user`)
         expect(failedRes.status).to.equal(401)
         expect(failedRes.text).to.equal(`Not logged in`)
-        expect(user).not.to.be.a(`null`)
-      })
-      it(`rejects attempts to delete users other than the logged in user`, async () => {
-        const failedRes = await agent2.delete(`/api/user/3`)
-        const user = await User.findByPk(3)
-        expect(failedRes.status).to.equal(401)
-        expect(failedRes.text).to.equal(`Permission denied`)
-        expect(user).not.to.be.a(`null`)
       })
       it(`redirects the user to the main page`, () => {
         expect(res.status).to.equal(302)
         expect(res.header[`location`]).to.equal(`/`)
+      })
+    })
+  })
+
+  describe(`/:id`, () => {
+    describe(`GET`, () => {
+      it(`returns the user and all their recipes`, async () => {
+        const res = await request(app).get(`/api/user/1`)
+        expect(res.status).to.equal(200)
+        expect(res.body.email).to.equal(`new@example.com`)
+        expect(res.body.recipes.length).to.equal(2)
       })
     })
   })
