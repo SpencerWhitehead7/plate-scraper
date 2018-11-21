ee25ef5d2c047ba50d6cc044dc690411bdd38edf
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-20 20:24:02 -0500

    feat: very basic routing skeleton

--- client/components/index.js
+++ client/components/index.js
@@ -1,91 +1,23 @@
 import React from 'react'
-import axios from 'axios'
+import {BrowserRouter, Switch, NavLink, Route} from 'react-router-dom'
 
-import RecipeArea from './RecipeArea'
-import SupportedSites from './SupportedSites'
-import UrlForm from './UrlForm'
-import Warning from './Warning'
+import {Affix} from 'antd'
 
-class Main extends React.Component{
-  constructor(){
-    super()
-    this.state = {
-      url : ``,
-      sourceSite : ``,
-      sourceUrl : ``,
-      title : ``,
-      recipe : ``,
-      err : {},
-      isLoading : false,
-    }
-  }
+import Scrape from './scrape'
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name] : event.target.value,
-    })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-    this.setState({
-      sourceSite : ``,
-      sourceUrl : ``,
-      title : ``,
-      recipe : ``,
-      err : {},
-      isLoading : true,
-    }, async () => {
-      try{
-        const {data} = await axios.post(`/api`, {url : this.state.url})
-        console.log(data)
-        if(data.message && data.name &&
-          !data.sourceSite && !data.sourceUrl && !data.title && !data.recipe){ // duck typing to check for errors
-          this.setState({err : data, isLoading : false})
-        }else{
-          this.setState({
-            sourceSite : data.sourceSite,
-            sourceUrl : data.sourceUrl,
-            title : data.title,
-            recipe : data.recipe,
-            err : {},
-            isLoading : false,
-          })
-        }
-      }catch(err){
-        console.log(err)
-        this.setState({
-          err,
-          isLoading : false,
-        })
-      }
-    })
-  }
-
-  render(){
-    return (
-      <div id="whole-page">
-        <h1 id="title">Plate Scraper!</h1>
-        <SupportedSites/>
-        <UrlForm
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-          url={this.state.url}
-        />
-        {this.state.isLoading && <div className="loading"/>}
-        {
-          this.state.recipe !== `` &&
-            <RecipeArea
-              handleChange={this.handleChange}
-              recipe={this.state.recipe}
-              title={this.state.title}
-              sourceSite={this.state.sourceSite}
-            />
-        }
-        {Object.keys(this.state.err).length > 0 && <Warning err={this.state.err.message}/>}
-      </div>
-    )
-  }
-}
+const Main = () => (
+  <BrowserRouter>
+    <React.Fragment>
+      <Affix offsetTop="0">
+        <nav>
+          <NavLink exact to="/">Scrape</NavLink>
+        </nav>
+      </Affix>
+      <Switch>
+        <Route exact path="/" component={Scrape}/>
+      </Switch>
+    </React.Fragment>
+  </BrowserRouter>
+)
 
 export default Main
