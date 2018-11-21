4602006c3c1f97b508a387f052f0404ce69b9a6f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-21 16:48:09 -0500

    refactor: simplifies font loading

--- client/index.js
+++ client/index.js
@@ -6,14 +6,6 @@ import {Provider} from 'react-redux'
 import store from './redux/store'
 import Main from './components'
 
-import WebFont from 'webfontloader'
-
-WebFont.load({
-  google : {
-    families : [`Open Sans`],
-  },
-})
-
 ReactDOM.render(
   <Provider store={store}>
     <Main/>
