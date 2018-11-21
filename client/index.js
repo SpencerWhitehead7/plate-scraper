41fa0a1e1dab09e67eaafcace3d0582abb60c96c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-20 20:21:08 -0500

    feat: adds redux to root app

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {Provider} from 'react-redux'
+import store from './redux/store'
 import Main from './components'
 
 import WebFont from 'webfontloader'
@@ -14,6 +15,8 @@ WebFont.load({
 })
 
 ReactDOM.render(
-  <Main/>,
+  <Provider store={store}>
+    <Main/>
+  </Provider>,
   document.getElementById(`root`)
 )
