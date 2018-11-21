66c56a87c66f2bad23956d9e30fc73895093514b
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-21 17:04:30 -0500

    refactor: modularizes NavBar

--- client/components/index.js
+++ client/components/index.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import {BrowserRouter, Switch, NavLink, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import {Affix} from 'antd'
 
+import NavBar from './NavBar'
 import Scrape from './scrape'
+import NoMatch from './noMatch'
 
 const Main = () => (
   <BrowserRouter>
     <React.Fragment>
-      <Affix offsetTop="0">
-        <nav>
-          <NavLink exact to="/">Scrape</NavLink>
-        </nav>
+      <Affix offsetTop={0}>
+        <NavBar/>
       </Affix>
       <Switch>
         <Route exact path="/" component={Scrape}/>
+        <Route component={NoMatch}/>
       </Switch>
     </React.Fragment>
   </BrowserRouter>
