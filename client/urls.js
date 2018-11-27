51e2cb915239fbda02c24c48347594292aa4d6c4
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-27 00:23:19 -0500

    feat: basic, unlogicked navbar

---
+++ client/urls.js
@@ -0,0 +1,4 @@
+export default {
+  noMatchBG : `https://firebasestorage.googleapis.com/v0/b/plate-scraper.appspot.com/o/404.png?alt=media&token=80f6defe-79d2-4310-97c6-206594250b43`,
+  logo : `https://firebasestorage.googleapis.com/v0/b/plate-scraper.appspot.com/o/favicon.png?alt=media&token=8d5df50e-f789-4080-8209-5528bb296dda`,
+}
