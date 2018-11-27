d22a3b14905f075e9a09d7f46779ff982eaba115
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-26 23:33:28 -0500

    feat: minimally styles 404 page

--- client/components/noMatch/index.js
+++ client/components/noMatch/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import s from './index.css'
+
 const noMatch = () => (
-  <h1>
-    404 Page Not Found:
-    <br/>
-    Oh no, there's nothing here!
-  </h1>
+  <div className={s.centerContainer}>
+    <h1 className={s.centerText}>
+      404 Page Not Found:
+      <br/>
+      Oh no, there's nothing here!
+    </h1>
+  </div>
 )
 
 export default noMatch
