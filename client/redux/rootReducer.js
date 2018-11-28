6461db87739ea08c9c5cadc65d47472aef6e3601
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-11-27 19:07:50 -0500

    feat: me thunk

--- client/redux/rootReducer.js
+++ client/redux/rootReducer.js
@@ -1,11 +1,35 @@
 // import { combineReducers} from 'redux'
+import axios from 'axios'
+import racf from 'redux-action-creator-factory'
 
 const initialState = {}
+
+// Action types
+const A = {
+  GET : `GET_USER`,
+}
+
+// Action creators
+const getUser = user => racf(A.GET, user)
+
+// Thunk creators
+export const me = () => async dispatch => {
+  try{
+    const {data} = await axios.get(`auth/me`)
+    console.log(data)
+    const user = data || initialState
+    dispatch(getUser(user))
+  }catch(err){
+    console.log(err)
+  }
+}
 
 // const reducer = combineReducers({}}
 
 const reducer = (state = initialState, action) => {
   switch(action.type){
+    case A.GET:
+      return action.user
     default:
       return state
   }
