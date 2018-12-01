b4fdc14518939ef7e14827e7a339e09c97307434
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-12-01 13:21:09 -0500

    fix: store creation with middleware/devtool

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,15 +1,15 @@
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {createLogger} from 'redux-logger'
-import axios from 'axios'
 import reducer from './rootReducer'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(
     applyMiddleware(
-      thunkMiddleware.withExtraArgument({axios}),
+      thunkMiddleware,
       createLogger()
     )
   )
