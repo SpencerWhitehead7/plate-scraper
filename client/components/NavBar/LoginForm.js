3807bc75a036960aa9d7d4a885356bb768e1b43f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-12-01 18:03:24 -0500

    feat: login form

---
+++ client/components/NavBar/LoginForm.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import {connect} from 'react-redux'
+
+import {auth} from '../../redux/rootReducer'
+
+import s from './NavBar.css'
+
+const LoginForm = props => {
+  const {toggleModal, user, loginError} = props
+  const [email, setEmail] = useState(``)
+  const [password, setPassword] = useState(``)
+  const [err, setErr] = useState({})
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    if(!email || !password){
+      setErr({missingField : true})
+    }else{
+      setErr({...loginError})
+      props.login(email, password)
+    }
+  }
+
+  return (
+    <>
+      {/* this jank bs closes modal on successful login */}
+      {user.id && toggleModal()}
+      {/* this jank bs sets error states in conjunction with redux */}
+      {loginError.status !== err.status && setErr({...err, ...loginError})}
+      {err.status &&
+      <span>
+        {`Error: ${err.status} ${err.statusText}`}
+        <br/>
+        {`${err.data}`}
+      </span>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          {err.missingField && !email && <span>Email Required</span>}
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={evt => setEmail(evt.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          {err.missingField && !password && <span>Password Required</span>}
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={evt => setPassword(evt.target.value)}
+          />
+        </label>
+        <button type="submit">
+          Login
+        </button>
+      </form>
+    </>
+  )
+}
+
+const mstp = state => ({
+  user : state.user,
+  loginError : state.loginError,
+})
+
+const mdtp = dispatch => ({
+  login : (email, password) => dispatch(auth(email, password, `login`)),
+})
+
+export default connect(mstp, mdtp)(LoginForm)
--- client/components/NavBar/Modal.js
+++ client/components/NavBar/Modal.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react'
 
+import LoginForm from './LoginForm'
+
 import s from './NavBar.css'
 
 const Modal = props => {
   const {toggleModal} = props
-  const [email, setEmail] = useState(``)
-  const [password, setPassword] = useState(``)
   return (
     <div
       className={s.modal}
@@ -15,13 +15,9 @@ const Modal = props => {
         className={s.modalMain}
         onClick={event => {event.stopPropagation()}}
       >
-        {/* forms for logging in and signing up also thunks somehow */}
-        <button
-          type="button"
-          onClick={toggleModal}
-        >
-          Close
-        </button>
+        <LoginForm
+          toggleModal={toggleModal}
+        />
       </section>
     </div>
   )
