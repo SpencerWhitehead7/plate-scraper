97c79feba3283909ae651940aed3c66105762c8c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-10 03:57:46 -0400

    res: change webpack for dev/prod modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,60 @@
-module.exports = {
-  entry: [`@babel/polyfill`, `./client/index.js`],
-  output: {
-    path: `${__dirname}/public`,
-    filename: `bundle.js`,
-  },
-  devtool: `source-maps`,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: `babel-loader`,
-          options: {
-            presets: [`@babel/preset-react`, `@babel/preset-env`],
+// eslint-disable-next-line no-unused-vars
+module.exports = (env, argv) => {
+  const isDev = argv.mode === `development`
+
+  return {
+    entry: [`@babel/polyfill`, `./client/index.js`],
+    output: {
+      path: `${__dirname}/public`,
+      filename: `bundle.js`,
+    },
+    devtool: isDev ? `eval-source-map` : `source-map`,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: `babel-loader`,
+            options: {
+              presets: [`@babel/preset-react`, `@babel/preset-env`],
+            },
           },
         },
-      },
-      {
-        test: /\.scss$/i,
-        use: [
-          `style-loader`,
-          {
-            loader: `css-loader`,
-            options: {
-              modules: {
-                localIdentName: `[path][name]_[local]---[hash:base64:5]`,
+        {
+          test: /\.scss$/i,
+          use: [
+            `style-loader`,
+            {
+              loader: `css-loader`,
+              options: {
+                modules: {
+                  localIdentName: `[path][name]_[local]---[hash:base64:5]`,
+                },
+                sourceMap: true,
+                esModule: true,
               },
-              sourceMap: true,
-              esModule: true,
             },
-          },
-          `sass-loader`,
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          `style-loader`,
-          {
-            loader: `css-loader`,
-            options: {
-              modules: {
-                localIdentName: `[path][name]_[local]-[hash:base64:5]`,
+            `sass-loader`,
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            `style-loader`,
+            {
+              loader: `css-loader`,
+              options: {
+                modules: {
+                  localIdentName: `[path][name]_[local]-[hash:base64:5]`,
+                },
+                sourceMap: true,
+                esModule: true,
               },
-              sourceMap: true,
-              esModule: true,
             },
-          },
-        ],
-      },
-    ],
-  },
+          ],
+        },
+      ],
+    },
+  }
 }
