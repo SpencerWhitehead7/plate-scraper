05288b50bd21784ea6cbb457f30024ddda0377e0
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-13 01:18:07 -0400

    refac: SupportedSites comp is dynamic, uses sitewide constant

--- client/feats/Scrape/SupportedSites.js
+++ client/feats/Scrape/SupportedSites.js
@@ -1,46 +1,39 @@
 import React from 'react'
 import classnames from 'classnames'
 
+import { SUPPORTED_SITES } from 'consts'
+
 import skele from 'skeleton.css'
 import sg from 'styles/index.scss'
 import s from './SupportedSites.scss'
 
-const SupportedSites = () => (
-  <section className={s.supportedSites}>
-    <h3 className={sg.textCenter}> Supported Sites</h3>
-    <div className={skele.container}>
-      <div className={skele.row}>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.allrecipes.com">allrecipes.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.bettycrocker.com">bettycrocker.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.bonappetit.com">bonappetit.com</a>
-      </div>
+const SupportedSites = () => {
+  const columnCount = 3
+  const rowCount = Math.ceil(SUPPORTED_SITES.length / columnCount)
+  const rows = SUPPORTED_SITES.reduce(
+    (rowsArr, site, i) => {
+      rowsArr[i % rowsArr.length].push(site)
+      return rowsArr
+    },
+    new Array(rowCount).fill(null).map(_ => []),
+  )
 
-      <div className="row">
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.chowhound.com">chowhound.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.cookinglight.com">cookinglight.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.eatingwell.com">eatingwell.com</a>
-      </div>
+  const Cell = ({ site }) => <a key={site} className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href={`https://www.${site}`}>{site}</a>
 
-      <div className="row">
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.epicurious.com">epicurious.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.food.com">food.com</a>
-        {/* <a className={classnames(skele[`one-third`],  skele.column, sg.textCenter)} href="https://www.food52.com">food52.com</a> uncomment if I ever get it working */}
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.foodandwine.com">foodandwine.com</a>
-      </div>
-
-      <div className="row">
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.foodnetwork.com">foodnetwork.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.jamieoliver.com">jamieoliver.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.myrecipes.com">myrecipes.com</a>
-      </div>
-
-      <div className="row">
-        {/* <a className={classnames(skele[`one-third`],  skele.column, sg.textCenter)} href="https://www.thekitchn.com">thekitchn.com</a> uncomment if I ever get it working */}
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.simplyrecipes.com">simplyrecipes.com</a>
-        <a className={classnames(skele[`one-third`], skele.column, sg.textCenter)} href="https://www.seriouseats.com/recipes">seriouseats.com/recipes</a>
-      </div>
+  const Row = ({ row }) => (
+    <div className="row">
+      {row.map(site => <Cell key={site} site={site} />)}
     </div>
-  </section>
-)
+  )
+
+  return (
+    <section className={s.supportedSites}>
+      <h3 className={sg.textCenter}> Supported Sites</h3>
+      <div className={skele.container}>
+        {rows.map(row => <Row key={row[0]} row={row} />)}
+      </div>
+    </section>
+  )
+}
 
 export default SupportedSites
