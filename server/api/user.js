73c1053c8747f38e85df7d26c6b2306977f08d04
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-20 22:53:14 -0400

    refac: move logged-in user actions from /user to /auth

--- server/api/user.js
+++ server/api/user.js
@@ -1,50 +1,6 @@
 const router = require(`express`).Router()
 const { User, Recipe, Tag } = require(`../db/models`)
 
-const { isAuthenticated } = require(`../authenticationLogic`)
-
-// PUT /api/user
-router.put(
-  `/`,
-  isAuthenticated,
-  async (req, res, next) => {
-    try {
-      const newInfo = JSON.parse(JSON.stringify(req.body.newInfo))
-      const user = await User.findByPk(req.user.id)
-      if (req.body.password && user.correctPassword(req.body.password)) {
-        delete newInfo.salt
-        delete newInfo.id
-        const [, updatedUser] = await User.update(newInfo, {
-          where: { id: req.user.id },
-          returning: true,
-          plain: true,
-        })
-        res.json(updatedUser)
-      } else {
-        res.sendStatus(401)
-      }
-    } catch (error) {
-      next(error)
-    }
-  },
-)
-
-// DELETE /api/user
-router.delete(
-  `/`,
-  isAuthenticated,
-  async (req, res, next) => {
-    try {
-      const user = await User.findByPk(req.user.id)
-      await user.destroy()
-      req.logout()
-      req.session.destroy(err => (err ? next(err) : res.sendStatus(200)))
-    } catch (error) {
-      next(error)
-    }
-  },
-)
-
 // GET /api/user/:id
 router.get(`/:id`, async (req, res, next) => {
   try {
--- server/auth/index.js
+++ server/auth/index.js
@@ -4,6 +4,56 @@ const User = require(`../db/models/user`)
 
 const { isAuthenticated, isAlreadyAuthenticated } = require(`../authenticationLogic`)
 
+// GET /auth
+router.get(`/`, (req, res, next) => {
+  res.json(req.user)
+})
+
+// PUT /auth
+router.put(
+  `/`,
+  isAuthenticated,
+  async (req, res, next) => {
+    try {
+      const { user, body } = req
+      // keep someone from modifying a field they shouldn't via curl or whatever
+      const { newEmail, newUserName, newPassword, password } = body
+      if (user.correctPassword(password)) {
+        const updatedUser = await user.update({
+          email: newEmail,
+          userName: newUserName,
+          password: newPassword,
+        }, {
+          where: { id: user.id },
+          returning: true,
+          plain: true,
+        })
+        res.json(updatedUser)
+      } else {
+        res.sendStatus(401)
+      }
+    } catch (error) {
+      next(error)
+    }
+  },
+)
+
+// DELETE /auth
+router.delete(
+  `/`,
+  isAuthenticated,
+  async (req, res, next) => {
+    try {
+      const { user } = req
+      await user.destroy()
+      req.logout()
+      req.session.destroy(err => (err ? next(err) : res.sendStatus(200)))
+    } catch (error) {
+      next(error)
+    }
+  },
+)
+
 // POST /auth/signup
 router.post(
   `/signup`,
@@ -50,10 +100,5 @@ router.post(
     req.session.destroy(err => (err ? next(err) : res.sendStatus(204)))
   },
 )
-
-// GET /auth/me
-router.get(`/me`, (req, res, next) => {
-  res.json(req.user)
-})
 
 module.exports = router
