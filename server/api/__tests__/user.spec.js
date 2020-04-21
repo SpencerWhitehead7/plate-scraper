6cdcc2f60f1491c605d686449fdff3836cef8f1f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-20 22:53:14 -0400

    refac: move logged-in user actions from /user to /auth

--- server/api/__tests__/user.spec.js
+++ server/api/__tests__/user.spec.js
@@ -6,7 +6,6 @@ const Recipe = require(`../../db`).model(`recipe`)
 const User = require(`../../db`).model(`user`)
 
 const agent1 = request.agent(app)
-const agent2 = request.agent(app)
 
 describe(`API Route User: /api/user`, () => {
   const userCred = {
@@ -14,41 +13,24 @@ describe(`API Route User: /api/user`, () => {
     password: `pw`,
     userName: `testUser`,
   }
-  const user2Cred = {
-    email: `testUser2@example.com`,
-    password: `pw`,
-    userName: `testUser2`,
-  }
 
   before(async () => {
     try {
       await Recipe.sync({ force: true })
       await User.sync({ force: true })
       await agent1.post(`/auth/signup`).send(userCred)
-      await agent2.post(`/auth/signup`).send(user2Cred)
       await agent1.post(`/api/recipe`).send({
         text: `text1`,
         title: `title1`,
       })
-      await agent1.post(`/api/recipe`).send({
-        text: `text2`,
-        title: `title2`,
-      })
       await agent1.post(`/api/tag`).send({ name: `testtag`, recipeId: 1 })
-      await agent2.post(`/api/recipe`).send({
-        text: `text3`,
-        title: `title3`,
-      })
     } catch (err) {
       console.log(err)
     }
   })
   after(async () => {
     try {
-      await Promise.all([
-        agent1.post(`/auth/logout`),
-        agent2.post(`/auth/logout`),
-      ])
+      await agent1.post(`/auth/logout`)
       await Recipe.sync({ force: true })
       await User.sync({ force: true })
     } catch (err) {
@@ -56,118 +38,16 @@ describe(`API Route User: /api/user`, () => {
     }
   })
 
-  describe(`/`, () => {
-    describe(`PUT`, () => {
-      let res = null
-      let oldSalt = null
-      let newSalt = null
-      before(async () => {
-        try {
-          let user = await User.findOne({ where: { email: `testUser@example.com` } })
-          oldSalt = user.dataValues.salt
-          res = await agent1.put(`/api/user`).send({
-            newInfo: {
-              email: `new@example.com`,
-              salt: `new salt`,
-              id: 10,
-            },
-            password: `pw`,
-          })
-          user = await User.findOne({ where: { email: `new@example.com` } })
-          newSalt = user.dataValues.salt
-        } catch (err) {
-          console.log(err)
-        }
-      })
-
-      it(`edits the logged in user`, async () => {
-        const user = await User.findOne({ where: { email: `new@example.com` } })
-        expect(user).not.to.be.a(`null`)
-      })
-      it(`rejects unauthenticated users`, async () => {
-        const failedRes = await request(app).put(`/api/user`).send({ email: `failed@example.com` })
-        const user = await User.findOne({ where: { email: `failed@example.com` } })
-        expect(failedRes.status).to.equal(401)
-        expect(failedRes.text).to.equal(`Not logged in`)
-        expect(user).to.be.a(`null`)
-      })
-      it(`requires the user to submit their password to edit their info`, async () => {
-        const failedRes1 = await agent1.put(`/api/user`).send({
-          newInfo: { email: `new2@example.com` },
-          password: `anIncorrectPassword`,
-        })
-        const failedRes2 = await agent2.put(`/api/user`).send({
-          newInfo: { email: `new3@example.com` },
-        })
-        const failedUser1 = await User.findOne({ where: { email: `new2@example.com` } })
-        const failedUser2 = await User.findOne({ where: { email: `new3@example.com` } })
-        expect(failedRes1.status).to.equal(401)
-        expect(failedRes2.status).to.equal(401)
-        expect(failedUser1).to.be.a(`null`)
-        expect(failedUser2).to.be.a(`null`)
-      })
-      it(`does not allow the user to edit their id`, () => {
-        expect(res.body.id).to.equal(1)
-      })
-      it(`does not allow the user to edit salt`, () => {
-        expect(oldSalt).to.equal(newSalt)
-      })
-      it(`returns the user`, () => {
-        expect(res.status).to.equal(200)
-        expect(res.body.id).to.equal(1)
-        expect(res.body.email).to.equal(`new@example.com`)
-      })
-    })
-
-    describe(`DELETE`, () => {
-      let res = null
-      before(async () => {
-        try {
-          res = await agent2.delete(`/api/user`)
-        } catch (error) {
-          console.log(error)
-        }
-      })
-      it(`deletes the logged in user and their recipes`, async () => {
-        const user = await User.findByPk(2)
-        const remaining = await Recipe.findAll()
-        expect(user).to.be.a(`null`)
-        expect(remaining.length).to.equal(2)
-      })
-      it(`logs the user out`, async () => {
-        const failedRes = await agent2.get(`/auth/me`)
-        expect(failedRes.body).to.equal(``)
-      })
-      it(`rejects unauthenticated users`, async () => {
-        const failedRes = await request(app).delete(`/api/user`)
-        expect(failedRes.status).to.equal(401)
-        expect(failedRes.text).to.equal(`Not logged in`)
-      })
-      it(`returns a 200`, () => {
-        expect(res.status).to.equal(200)
-      })
-    })
-  })
-
   describe(`/:id`, () => {
-    let res = null
-    before(async () => {
-      try {
-        res = await request(app).get(`/api/user/1`)
-      } catch (err) {
-        console.log(err)
-      }
-    })
     describe(`GET`, () => {
-      it(`returns the user and all their recipes and all their recipes' tags`, () => {
+      it(`returns the user, their recipes, and their recipes' tags`, async () => {
+        const res = await request(app).get(`/api/user/1`)
         expect(res.status).to.equal(200)
-        expect(res.body.email).to.equal(`new@example.com`)
-      })
-      it(`and all their recipes`, () => {
-        expect(res.body.recipes.length).to.equal(2)
-      })
-      it(`and all their recipes' tags`, () => {
-        expect(res.body.recipes[0].tags.length).to.equal(1)
+        expect(res.body.id).to.equal(1)
+        expect(res.body.email).to.equal(`testUser@example.com`)
+        expect(res.body.userName).to.equal(`testUser`)
+        expect(res.body.recipes).to.have.lengthOf(1)
+        expect(res.body.recipes[0].tags).to.have.lengthOf(1)
       })
     })
   })
--- server/auth/__tests__/auth.spec.js
+++ server/auth/__tests__/auth.spec.js
@@ -30,15 +30,88 @@ describe(`Auth Route: /auth`, () => {
     }
   })
 
+  describe(`/`, () => {
+    describe(`GET`, () => {
+      it(`if the user is not logged in, it returns 200 with no body`, async () => {
+        const res = await request(app).get(`/auth`)
+        expect(res.status).to.equal(200)
+        expect(res.body).to.equal(``)
+      })
+      it(`if the user is logged in, it returns 200 with the user's information`, async () => {
+        const res = await agent.get(`/auth`)
+        expect(res.status).to.equal(200)
+        expect(res.body.id).to.equal(1)
+      })
+    })
+
+    describe(`PUT`, () => {
+      it(`edits the logged in user`, async () => {
+        const oldUser = await User.findByPk(1)
+        const newEmail = `new@example.com`
+        const newUserName = `newUserName`
+        const newPassword = `newpw`
+        await agent.put(`/auth`).send({
+          newEmail,
+          newUserName,
+          newPassword,
+          password: userCred.password,
+        })
+        const editedUser = await User.findByPk(1)
+        expect(editedUser.email).not.to.equal(oldUser.email)
+        expect(editedUser.userName).not.to.equal(oldUser.userName)
+        expect(editedUser.password()).not.to.equal(oldUser.password())
+        expect(editedUser.email).to.equal(newEmail)
+        expect(editedUser.userName).to.equal(newUserName)
+        expect(editedUser.correctPassword(newPassword)).to.be.true
+      })
+      it(`returns a 401 and does not edit the user if the user sends the wrong confirmation password`, async () => {
+        const newEmail = `new@example.com`
+        const res = await agent.put(`/auth`).send({ newEmail, password: `wrongpw` })
+        const user = await User.findByPk(1)
+        expect(user.email).not.to.equal(newEmail)
+        expect(res.status).to.equal(401)
+        expect(res.text).to.equal(`Unauthorized`)
+      })
+      it(`returns a 401 if the user is not logged in`, async () => {
+        const res = await request(app).put(`/auth`).send({ newEmail: `new@example.com`, password: `pw` })
+        expect(res.status).to.equal(401)
+        expect(res.text).to.equal(`Not logged in`)
+      })
+    })
+
+    describe(`DELETE`, () => {
+      it(`deletes the logged in user `, async () => {
+        const res = await agent.delete(`/auth`)
+        const user = await User.findByPk(1)
+        expect(res.status).to.equal(200)
+        expect(user).to.be.null
+      })
+      it(`logs out the logged in user`, async () => {
+        await agent.delete(`/auth`)
+        const res = await agent.get(`/auth`)
+        expect(res.body).to.equal(``)
+      })
+      it(`returns a 401 if the user is not logged in`, async () => {
+        const res = await request(app).delete(`/auth`)
+        expect(res.status).to.equal(401)
+        expect(res.text).to.equal(`Not logged in`)
+      })
+    })
+  })
+
   describe(`/signup`, () => {
     describe(`POST`, () => {
       it(`creates a user with the correct credentials in the database`, async () => {
-        const testUser = await User.findOne({ where: { email: userCred.email } })
+        const testUser = await User.findByPk(1)
         expect(testUser.email).to.equal(userCred.email)
+        expect(testUser.userName).to.equal(userCred.userName)
+        expect(testUser.correctPassword(userCred.password)).to.be.true
       })
       it(`also logs that user into the app`, async () => {
-        const res = await agent.get(`/auth/me`)
-        expect(res.status).to.equal(200)
+        const res = await agent.get(`/auth`)
+        expect(res.body.id).to.equal(1)
+        expect(res.body.email).to.equal(userCred.email)
+        expect(res.body.userName).to.equal(userCred.userName)
       })
       it(`returns a 409 if the user is already logged in`, async () => {
         const res = await agent.post(`/auth/signup`).send(userCred)
@@ -56,15 +129,14 @@ describe(`Auth Route: /auth`, () => {
   describe(`/login`, () => {
     describe(`POST`, () => {
       it(`logs the user in if they provide correct credentials`, async () => {
-        // using a one-off results in a lasting row in the sessions db
         await agent.post(`/auth/logout`)
         const res = await agent.post(`/auth/login`).send(userCred)
         expect(res.status).to.equal(200)
         expect(res.body.id).to.equal(1)
-        expect(res.body.email).to.equal(`testUser@example.com`)
       })
       it(`returns a 401 if the user provides incorrect credentials`, async () => {
-        const res = await request(app).post(`/auth/login`).send({ email: `testUser@example.com`, password: `wrongpw` })
+        await agent.post(`/auth/logout`)
+        const res = await agent.post(`/auth/login`).send({ email: `testUser@example.com`, password: `wrongpw` })
         expect(res.status).to.equal(401)
         expect(res.text).to.equal(`Wrong username or password`)
       })
@@ -80,7 +152,7 @@ describe(`Auth Route: /auth`, () => {
     describe(`POST`, () => {
       it(`logs the user out if the user is logged in`, async () => {
         await agent.post(`/auth/logout`)
-        const res = await agent.get(`/auth/me`)
+        const res = await agent.get(`/auth`)
         expect(res.body).to.equal(``)
       })
       xit(`destroy's the user's session if the user is logged in`, async () => {
@@ -95,22 +167,6 @@ describe(`Auth Route: /auth`, () => {
         const res = await request(app).post(`/auth/logout`)
         expect(res.status).to.equal(401)
         expect(res.text).to.equal(`Not logged in`)
-      })
-    })
-  })
-
-  describe(`the /me route`, () => {
-    describe(`GET`, () => {
-      it(`if the user is not logged in, it returns 200 with no body`, async () => {
-        const res = await request(app).get(`/auth/me`)
-        expect(res.status).to.equal(200)
-        expect(res.body).to.equal(``)
-      })
-      it(`if the user is logged in, it returns 200 with the user's information`, async () => {
-        const res = await agent.get(`/auth/me`)
-        expect(res.status).to.equal(200)
-        expect(res.body.id).to.equal(1)
-        expect(res.body.email).to.equal(`testUser@example.com`)
       })
     })
   })
