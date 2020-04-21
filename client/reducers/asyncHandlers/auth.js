a9ff5ec583df6accb08e38b9de439468753d4f1f
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-04-21 00:49:52 -0400

    feat: destroy accont

--- client/reducers/asyncHandlers/auth.js
+++ client/reducers/asyncHandlers/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import buildAsyncHandler from './asyncHandlerBuilder'
 
-const asyncFnToHandle = async ({ email, password, userName, newEmail, newPassword, newUserName, isLogout } = {}) => {
+const asyncFnToHandle = async ({ email, password, userName, newEmail, newPassword, newUserName, isLogout, isDestroy } = {}) => {
   let data
   if (email && userName && password) {
     ({ data } = await axios.post(`/api/auth/signup`, { email, userName, password }))
@@ -9,12 +9,17 @@ const asyncFnToHandle = async ({ email, password, userName, newEmail, newPasswor
     ({ data } = await axios.post(`/api/auth/login`, { email, password }))
   } else if (password && (newEmail || newUserName || newPassword)) {
     ({ data } = await axios.put(`/api/auth/`, { password, newEmail, newUserName, newPassword }))
-  } else if (!isLogout) {
+  } else if (!isLogout && !isDestroy) {
     ({ data } = await axios.get(`/api/auth`))
   }
 
   if (isLogout) {
     await axios.post(`/api/auth/logout`)
+    return null
+  }
+
+  if (isDestroy && password) {
+    await axios.post(`/api/auth/destroy`, { password })
     return null
   }
 
