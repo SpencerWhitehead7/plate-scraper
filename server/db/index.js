be9a53355c8bf45495b49d03cb83e16f6b3aec7d
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-05-03 21:52:22 -0400

    edit: slightly rationalize DB

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,10 @@
-const database = require(`./database`)
+const Sequelize = require(`sequelize`)
+const pkg = require(`../../package.json`)
 
-// register models
-require(`./models`)
+const databaseName = pkg.name + (process.env.NODE_ENV === `test` ? `-test` : ``)
+
+const database = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, {
+  logging: false,
+})
 
 module.exports = database
--- server/db/models/recipe.js
+++ server/db/models/recipe.js
@@ -1,5 +1,5 @@
 const Sequelize = require(`sequelize`)
-const database = require(`../database`)
+const database = require(`../`)
 
 const Recipe = database.define(`recipe`, {
   text: { // the actual recipe
