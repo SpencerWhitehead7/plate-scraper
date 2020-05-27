f5180a40bc4f79e4139f75e916bc16bcc0ba563c
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-05-27 01:13:26 -0400

    edit: dead code

--- client/feats/Account/Account.js
+++ client/feats/Account/Account.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Link, useParams, useLocation } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 
@@ -18,6 +18,8 @@ const Account = ({ dataToDisplay, fetchUser, isLoading, isMyPage, logout, userId
     fetchUser(userId)
   }, [userId, fetchUser])
 
+  useContainer(containerFn)
+
   return (
     isLoading
       ? <LoadingIndicator /> :
@@ -27,6 +29,13 @@ const Account = ({ dataToDisplay, fetchUser, isLoading, isMyPage, logout, userId
             <CardTitle>{dataToDisplay.userName}</CardTitle>
             <CardSubtitle>Recipes</CardSubtitle>
             <RecipeRows recipes={dataToDisplay.recipes} />
+            <Link to="/user/3">
+              to 3
+            </Link>
+
+            <Link to="/user/2">
+              to 2
+            </Link>
           </Card>
 
           {isMyPage && (
@@ -57,6 +66,17 @@ const Account = ({ dataToDisplay, fetchUser, isLoading, isMyPage, logout, userId
   )
 }
 
+const useContainer = containerFn => {
+  const location = useLocation() // process numbers
+  const params = useParams() // process querystring into a real obj with qs, process numbers
+
+  containerFn({ location, params })
+}
+
+const containerFn = args => {
+  console.log(args)
+}
+
 const mstp = (state, ownProps) => {
   const { userId: userIdStr } = ownProps.match.params
   const userId = Number(userIdStr)
@@ -80,3 +100,15 @@ const mdtp = dispatch => ({
 })
 
 export default compose(withRouter, connect(mstp, mdtp))(Account)
+
+// maybe a use container hook
+// it'll get and process the route params and query for you via another hook that uses those hooks
+// you'll pass it a function that does all the container stuff your container needs, likely composed of a bunch of react-redux hooks and suchlike
+// then it'll just run that and pass it all the params everyone gets.
+// If the inner fn needs other params, it can gather them itself
+// The useContainer hook will be invoked from a container comp; its return value(s) will be passed to the contained child as props
+
+// maybe make a bunch of reselect-y selector functions with react-redux select hooks/etc?
+
+// Another useAsync stuff hook that will be responsible for kicking off async stuff via useEffect hooks
+// useContainer will invoke both performAsync and getProps if necessary, if no async actions depend on route, just getProps
