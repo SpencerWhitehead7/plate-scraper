39ea78d862058cc84e4f2c09bd6a66d810277b35
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-06-01 12:27:06 -0400

    refac: scrape forms use standard comps

---
+++ client/feats/Scrape/RecipeForm.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { connect } from 'react-redux'
+
+import { scrapeAsyncHandler } from 'reducers/asyncHandlers'
+import { FormAutosizingTextarea, Submit, Warning } from 'comps/Form'
+import LoadingIndicator from 'comps/LoadingIndicator'
+
+import s from './RecipeForm.scss'
+
+const RecipeForm = ({ data, error, isLoaded, isLoading }) => {
+  const { errors, formState, handleSubmit, register, watch } = useForm({ mode: `onChange` })
+
+  const onSubmit = ({ text, title }) => {
+    const textAsBlob = new Blob([text], { type: `text/plain` })
+    const downloadLink = document.createElement(`a`)
+    downloadLink.download = title
+    downloadLink.href = window.URL.createObjectURL(textAsBlob)
+    downloadLink.onclick = evt => { document.body.removeChild(evt.target) }
+    downloadLink.style.display = `none`
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    // eslint-disable-next-line no-alert
+    alert(`Saved to your default download location`)
+  }
+
+  return (
+    isLoading
+      ? <LoadingIndicator />
+      : data ? (
+        <form onSubmit={handleSubmit(onSubmit)} className={s.recipeForm}>
+          <FormAutosizingTextarea
+            identifier="text"
+            labelText="Recipe"
+            register={register}
+            registerOptions={{ required: true }}
+            watch={watch}
+            errors={errors}
+            defaultValue={isLoaded ? data.recipe : ``}
+          />
+
+          <label htmlFor="title">
+            Filename
+            <Warning rhfError={errors.title} />
+          </label>
+          <div className={s.recipeForm__inputBar}>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              defaultValue={isLoaded ? data.title : ``}
+              className={s.recipeForm__input}
+              ref={register({ required: true })}
+            />
+            <Submit value="Download!" formState={formState} />
+          </div>
+        </form>
+      )
+        : <Warning leftPadded={false} customError={error} />
+  )
+}
+
+const mstp = state => ({
+  ...scrapeAsyncHandler.select(state),
+})
+
+export default connect(mstp, null)(RecipeForm)
--- client/feats/Scrape/Scrape.js
+++ client/feats/Scrape/Scrape.js
@@ -1,71 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 
-import { scrapeAsyncHandler } from 'reducers/asyncHandlers'
-import AutosizingTextarea from 'comps/AutosizingTextarea'
 import Card from 'comps/Card'
-import LoadingIndicator from 'comps/LoadingIndicator'
-import DownloadForm from './DownloadForm'
+import RecipeForm from './RecipeForm'
 import SupportedSites from './SupportedSites'
 import UrlForm from './UrlForm'
-import Warning from './Warning'
 
-const Scrape = ({ error, isLoading, data: initialRecipe, scrape }) => {
-  const [url, setUrl] = useState(``)
-  const [recipe, setRecipe] = useState(initialRecipe)
+const Scrape = () => (
+  <Card>
+    <SupportedSites />
+    <UrlForm />
+    <RecipeForm />
+  </Card>
+)
 
-  const handleChangeUrl = evt => {
-    setUrl(evt.target.value)
-  }
-  const handleSubmit = evt => {
-    evt.preventDefault()
-    scrape(url)
-  }
-
-  useEffect(() => {
-    setRecipe(initialRecipe)
-  }, [initialRecipe])
-
-  const handleChangeRecipeText = evt => {
-    setRecipe({
-      ...recipe,
-      recipe: evt.target.value,
-    })
-  }
-
-  return (
-    <Card>
-      <SupportedSites />
-      <UrlForm
-        handleChange={handleChangeUrl}
-        handleSubmit={handleSubmit}
-        url={url}
-      />
-      {isLoading && <LoadingIndicator />}
-      {
-        !isLoading && recipe && Object.entries(recipe).length && (
-          <>
-            <AutosizingTextarea
-              name="recipe"
-              value={recipe.recipe || ``}
-              onChange={handleChangeRecipeText}
-            />
-            <DownloadForm {...recipe} />
-          </>
-        )
-      }
-      {error && <Warning error={error} />}
-    </Card>
-  )
-}
-
-const mstp = state => ({
-  ...scrapeAsyncHandler.select(state),
-})
-
-
-const mdtp = dispatch => ({
-  scrape: url => dispatch(scrapeAsyncHandler.call(url)),
-})
-
-export default connect(mstp, mdtp)(Scrape)
+export default Scrape
