25c507cd79444ddfea1b49afad859778f8ac5d90
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2020-06-02 15:03:53 -0400

    edit: dead code

--- server/db/repositories/tag.ts
+++ server/db/repositories/tag.ts
@@ -3,33 +3,10 @@ import {
   AbstractRepository,
   getCustomRepository,
 } from "typeorm";
-import { Tag, Recipe } from "../entities";
+import { Tag } from "../entities";
 
 @EntityRepository(Tag)
 class TagRepository extends AbstractRepository<Tag> {
-  async insert(name: string, recipes: Recipe[]) {
-    let tag = await this.createQueryBuilder("tag")
-      .select()
-      .where("name = :name", { name })
-      .getOne();
-
-    if (!tag) {
-      ({
-        raw: [tag],
-      } = await this.createQueryBuilder("tag")
-        .insert()
-        .into(Tag)
-        .values({ name, recipes })
-        .returning("*")
-        .execute());
-    }
-
-    await this.createQueryBuilder("tag")
-      .relation(Tag, "recipes")
-      .of(tag)
-      .add(recipes);
-  }
-
   async getOrInsert(tagNames: string[]) {
     if (tagNames.length) {
       await this.createQueryBuilder("tag")
@@ -45,13 +22,6 @@ class TagRepository extends AbstractRepository<Tag> {
     } else {
       return [];
     }
-  }
-
-  remove(tagName: string, recipes: Recipe[]) {
-    return this.createQueryBuilder("tag")
-      .relation(Tag, "recipes")
-      .of(tagName)
-      .remove(recipes);
   }
 }
 
