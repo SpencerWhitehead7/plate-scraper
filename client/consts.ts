350a1514785b915cf38287270aafeda82f04a157
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2022-12-18 00:06:38 -0500

    refac: remove old api interface code

---
+++ client/consts.ts
@@ -0,0 +1,40 @@
+import qs from 'qs'
+
+export const SUPPORTED_SITES = [
+  `allrecipes.com`,
+  `bettycrocker.com`,
+  `bonappetit.com`,
+  `chowhound.com`,
+  `cookinglight.com`,
+  `eatingwell.com`,
+  `epicurious.com`,
+  `food.com`,
+  // `food52.com`,
+  `foodandwine.com`,
+  `foodnetwork.com`,
+  `jamieoliver.com`,
+  `myrecipes.com`,
+  `seriouseats.com/recipes`,
+  `simplyrecipes.com`,
+  // `thekitchn.com`,
+]
+
+export const URL = {
+  base: `/`,
+  scrape: `/scrape`,
+  upload: `/upload`,
+  user: userId => `/user/${userId}`,
+  recipe: recipeId => `/recipe/${recipeId}`,
+  search: (tags = []) => (tags.length
+    ? `/search${qs.stringify(tags, { addQueryPrefix: true })}`
+    : `/search`),
+}
+
+export const PATH = {
+  base: `/`,
+  scrape: `/scrape`,
+  upload: `/upload`,
+  user: `/user/:userId`,
+  recipe: `/recipe/:recipeId`,
+  search: `/search`,
+}
